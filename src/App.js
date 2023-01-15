a6a74d91dab7c191ecb23c23a05eaf466d56ef6e
Aurelia Segarra <av.segarra@gmail.com>
2023-01-15 17:41:28 +0100

    initial commit - structure OK

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black p-4">
+      <h1 className="text-4xl text-slate-50">Celsius converter project</h1>
+    </div>
+  );
+}
+
+export default App;
